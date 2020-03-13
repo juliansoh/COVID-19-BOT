60d0218fb7bc9799cf5a04855f886f21ab156579
Joe Sarabia <josarab@microsoft.com>
2020-03-12 22:36:22 -0700

    Create create-kb.ps1

---
+++ utlities/create-kb.ps1
@@ -0,0 +1,39 @@
+$kbAppEndpoint = "<replace-me>.azurewebsites.net"
+$kbAppKey = "xxxxxxxxxx"
+
+$kbName = "covid-test5"
+
+$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$headers.Add("Content-Type", "application/json")
+$headers.Add("Ocp-Apim-Subscription-Key", $kbAppKey)
+
+$body = "{`"name`": `"$kbName`",`"urls`": [`"https://www.cdc.gov/coronavirus/2019-ncov/faq.html`"]}"
+
+$createResponse = Invoke-RestMethod 'https://westus.api.cognitive.microsoft.com/qnamaker/v4.0/knowledgebases/create' -Method 'POST' -Headers $headers -Body $body
+$createResponse | ConvertTo-Json
+
+Do {
+Write-Host "waiting to check status of create operation..."
+Start-Sleep -s 15
+$createStatusResponse = Invoke-RestMethod "https://westus.api.cognitive.microsoft.com/qnamaker/v4.0/operations/$($createResponse.operationId)" -Method 'GET' -Headers $headers
+$createStatusResponse | ConvertTo-Json
+}
+While ($createStatusResponse.operationState -eq "NotStarted" -or $createStatusResponse.operationState -eq "Running")
+
+If ($createStatusResponse.operationState -eq "Succeeded")
+{
+    #publish the kb, investigate invoke-webrequest to catch 204 (processed, but no response code)
+    $publishResponse = Invoke-RestMethod "https://westus.api.cognitive.microsoft.com/qnamaker/v4.0/$($createStatusResponse.resourceLocation)" -Method 'POST' -Headers $headers
+    $publishResponse | ConvertTo-Json
+
+
+    $runtimeKeyResponse = Invoke-RestMethod 'https://westus.api.cognitive.microsoft.com/qnamaker/v4.0/endpointkeys' -Method 'GET' -Headers $headers
+    $runtimeKeyResponse | ConvertTo-Json
+
+    $question = "{`"question`": `"Should I wear a mask?`"} "
+
+    $headers.Remove("Ocp-Apim-Subscription-Key")
+    $headers.Add("Authorization", "EndpointKey $($runtimeKeyResponse.primaryEndpointKey)")
+    $questionResponse = Invoke-RestMethod "https://$kbAppEndpoint/qnamaker$($createStatusResponse.resourceLocation)/generateAnswer" -Method 'POST' -Headers $headers -Body $question
+    $questionResponse | ConvertTo-Json
+}
